a9ac52f60656ddd280ace64b2199475466e14224
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2022-04-10 01:07:19 -0500

    agregando eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,18 @@ module "vpc" {
   version = "0.0.10"
   name    = var.name
 }
+
+module "eks" {
+  source  = "punkerside/eks/aws"
+  version = "0.0.5"
+
+  name        = var.name
+  eks_version = "1.22"
+
+  subnet_public_ids  = module.vpc.subnet_public_ids
+  subnet_private_ids = module.vpc.subnet_private_ids
+
+  tags = {
+    Name = var.name
+  }
+}
